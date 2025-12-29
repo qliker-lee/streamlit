535d242991fd46ce8b06f24e177ed95702537228
qliker-lee <qliker@kakao.com>
2025-12-30 03:34:50 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ CURRENT_DIR = Path(__file__).resolve()
 # streamlit_app.py가 QDQM 루트에 있으므로 parent를 사용
 PROJECT_ROOT = CURRENT_DIR.parent
 # 여러 가능한 경로 시도 (로컬/Cloud 환경 대응)
-IMAGE_DIR = PROJECT_ROOT / "streamlit"/"DataSense" / "DS_Output" / "images"
+IMAGE_DIR = PROJECT_ROOT /"DataSense" / "DS_Output" / "images"
 IMAGE_DIR2 = PROJECT_ROOT.parent / "DataSense" / "DS_Output" / "images"
 IMAGE_DIR3 = PROJECT_ROOT / "QDQM" / "DataSense" / "DS_Output" / "images" 
 
@@ -134,8 +134,8 @@ def download_solution_pdf():
     # 여러 가능한 경로를 순차적으로 시도
     pdf_paths = [
         IMAGE_DIR / "DataSense_Solution_Overview.pdf",
-        IMAGE_DIR2 / "DataSense_Solution_Overview.pdf",
-        IMAGE_DIR3 / "DataSense_Solution_Overview.pdf",
+        # IMAGE_DIR2 / "DataSense_Solution_Overview.pdf",
+        # IMAGE_DIR3 / "DataSense_Solution_Overview.pdf",
     ]
     
     pdf_found = None
